ef533ee2ea672d6a3b7bf7a89da6e5cb07b6a8bd
Emil Ebert <emilebert@icloud.com>
2024-12-01 14:56:22 +0100

    first puzzle

---
+++ 2024/1/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const readline = require('readline')
+const inputFile = "input.txt"
+
+let list_a = []
+let list_b = []
+
+function parse_input() {
+    return new Promise((resolve, reject) => {
+        const readStream = fs.createReadStream(inputFile, {encoding: 'utf8'})
+        const lineReader = readline.createInterface({input: readStream})
+
+        lineReader.on('line', (line) => {
+            const split = line.split(' ')
+            list_a.push(split[0])
+            list_b.push(split[3])
+        })
+
+        lineReader.on('close', () => {
+            resolve()
+        })
+
+        lineReader.on('error', (err) => {
+            reject(err)
+        })
+    })
+}
+
+parse_input().then(() => {
+    let result = 0;
+    list_a.sort();
+    list_b.sort();
+
+    for (let i = 0; i < list_a.length; i++) {
+        result += Math.abs(list_a[i] - list_b[i]);
+    }
+
+    console.log(result);
+})
+
